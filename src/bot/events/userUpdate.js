ddb631f05f0b45017247fae4307696225310717c
krushna <69315835+krushna06@users.noreply.github.com>
2021-07-29 09:21:04 +0530

    krushna

--- src/bot/events/userUpdate.js
+++
@@ -1,8 +0,0 @@
-const { Event } = require('klasa');
-
-module.exports = class extends Event {
-    run(oldUser, newUser) {
-        if (oldUser.bot && oldUser.username !== newUser.username && newUser.username) 
-            Bots.updateOne({ botid: newUser.id }, {$set: {username: newUser.username}});
-    }
-};
